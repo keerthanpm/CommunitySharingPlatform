af9393010f64a0105d4c47fbf6c81ddf5bf72be5
b8ibmjava18@iiht.tech <b8ibmjava18@iiht.tech>
2019-11-20 17:48:33 +0530

    Interceptor Service Working...:)

--- FSDAngular/src/app/app.module.ts
+++ FSDAngular/src/app/app.module.ts
@@ -10,13 +10,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { Layout1Component } from './layout/layout1/layout1.component';
 import { Layout1Module } from './layout/layout1/layout1.module';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { NewArticleComponent } from './modules/new-article/new-article.component';
 import { SettingsComponent } from './modules/settings/settings.component';
+import { BasicAuthHtppInterceptorService } from './service/basic-auth-htpp-interceptor.service';
 
 
 @NgModule({
@@ -37,7 +38,7 @@ import { SettingsComponent } from './modules/settings/settings.component';
     
 
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: BasicAuthHtppInterceptorService, multi: true }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
