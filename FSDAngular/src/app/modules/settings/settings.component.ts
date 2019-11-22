b7c7d2741d5ac4d536118f4fe7ff6bc30e652dd1
Iyaaz Hassan Mohtisham <iyaazhassanmohtisham@Iyaazs-MacBook-Air.local>
2019-11-23 02:10:49 +0530

    All the double clicks reloading of likes and delete : Resolved + Added Sign In error message on invalid credentials + updated the prefecting of tags+ added another layer of security on update settings (password+disabled fields that cannot be updated)

--- FSDAngular/src/app/modules/settings/settings.component.ts
+++ FSDAngular/src/app/modules/settings/settings.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder,Validators, FormGroup } from '@angular/forms';
 import { User2 } from 'src/app/service/signup.service';
 import { SettingService } from 'src/app/service/setting.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
 
 @Component({
   selector: 'app-settings',
@@ -10,7 +11,7 @@ import { SettingService } from 'src/app/service/setting.service';
 }) 
 export class SettingsComponent implements OnInit {
   
-
+  errorstring=""
   user:User2=new User2("","","","","");
   settingsForm:FormGroup;
   urlphoto:string="";
@@ -25,11 +26,11 @@ get userName(){
   return this.settingsForm.get('userName1');
 }
 
-constructor(private fb: FormBuilder,private settingService:SettingService ){
+constructor(private fb: FormBuilder,private settingService:SettingService, private loginService:AuthenticationService ){
   this.settingsForm = fb.group({
     'email':['',[Validators.required, Validators.email]],
     'username':['',[Validators.required, Validators.minLength(3)]],
-    'password':['',[Validators.required, Validators.minLength(8)]],
+    'password':['',[Validators.required, Validators.minLength(3)]],
     'url':[''],
     'bio':['']
     });
@@ -45,11 +46,18 @@ onsettings(post){
   
  this.user.url=this.settingsForm.value.url
  this.user.bio=this.settingsForm.value.bio
-//  console.log("After")
-//  console.log(this.user)
- this.settingService.updateuserdata(this.user).subscribe();
+//authenticate before
+this.loginService.authenticate(this.user.username,this.settingsForm.value.password).subscribe(response=>{
+  this.settingService.updateuserdata(this.user).subscribe();
+  this.errorstring=""
   // this.data.changeMessage(this.settingsForm.value.userName1);
   alert('Update succesful');
+},error=>{
+  this.errorstring="Invalid Credentials! Please try again"
+})
+//  this.settingService.updateuserdata(this.user).subscribe();
+//   // this.data.changeMessage(this.settingsForm.value.userName1);
+//   alert('Update succesful');
   
 }
 getuserdata(){
