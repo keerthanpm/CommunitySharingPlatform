ee1e0038e5a35a1a35dff8d595591146881fca0b
arjunsuvarna1 <arjunsuvarna.s@gmail.com>
2019-11-22 17:01:53 +0530

    fixed new article

--- FSDAngular/src/app/newarticle.service.ts
+++ FSDAngular/src/app/newarticle.service.ts
@@ -6,13 +6,14 @@ import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
 export class NewarticleService {
   private REST_LIKE_ROUTE = "http://localhost:4000/thread/create";
   constructor(private httpClient: HttpClient) { }
-  public postThread(title, post){
+  public postThread(title, post, tag){
     //const headers = new HttpHeaders({ Authorization: 'Bearer ' + sessionStorage.getItem('token') });
     console.log('token:'+sessionStorage.getItem('token'));
     let body = {
       title: title,
       post: post,
-      userId: sessionStorage.getItem("username")
+      userId: sessionStorage.getItem("username"),
+      tags: tag
     }
     
     this.httpClient.post(this.REST_LIKE_ROUTE, body).subscribe();
