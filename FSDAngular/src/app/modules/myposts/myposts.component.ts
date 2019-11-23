87a79ded63dc145e4e7a915453a2c17554da5f8c
Iyaaz Hassan Mohtisham <iyaazhassanmohtisham@Iyaazs-MacBook-Air.local>
2019-11-23 16:56:27 +0530

    Individual post fetching image and depending on user comment fetching image:Resolved

--- FSDAngular/src/app/modules/myposts/myposts.component.ts
+++ FSDAngular/src/app/modules/myposts/myposts.component.ts
@@ -31,6 +31,7 @@ export class MypostsComponent implements OnInit {
 
   ngOnInit() {
     this.getdata();
+    
   }
 
   editarticle(id){
@@ -55,7 +56,8 @@ export class MypostsComponent implements OnInit {
   getdata(){
     this.myfeedservice.sendGetRequest().subscribe((data: any[])=>{
       for(let user of data){
-  
+        console.log(user)
+        
         this.username=user.username
         
         this.settingService.getuserdata(this.username).subscribe(response=>{
