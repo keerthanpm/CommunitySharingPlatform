9af27ff2501ea5c1165f1bc9c4f050cdb4158aed
Iyaaz Hassan Mohtisham <iyaazhassanmohtisham@Iyaazs-MacBook-Air.local>
2019-11-23 15:34:44 +0530

    All worksgit add .

--- FSDAngular/src/app/app.module.ts
+++ FSDAngular/src/app/app.module.ts
@@ -12,10 +12,9 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
-import { Layout1Component } from './layout/layout1/layout1.component';
+
 import { Layout1Module } from './layout/layout1/layout1.module';
-import { DashboardComponent } from './modules/dashboard/dashboard.component';
-import { NewArticleComponent } from './modules/new-article/new-article.component';
+
 import { SettingsComponent } from './modules/settings/settings.component';
 import { BasicAuthHtppInterceptorService } from './service/basic-auth-htpp-interceptor.service';
 import { ArticleComponent } from './modules/article/article.component';
