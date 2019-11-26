63e0e820f5fff186796a0db3bbd554a6ee87233c
arjunsuvarna1 <arjunsuvarna.s@gmail.com>
2019-11-26 13:00:35 +0530

    Revert "pushing"
    This reverts commit 3f33a28407a9c4fd083b38c5a0f07e4f996327a7, reversing
    changes made to b0fc65d73d65857956f8bc15905ade916cbd160e.

--- FSDAngular/src/app/shared/components/search/search.component.ts
+++ FSDAngular/src/app/shared/components/search/search.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from 'src/app/service/search.service';
-import { ConcatSource } from 'webpack-sources';
-import { SettingService } from 'src/app/service/setting.service';
 
 @Component({
   selector: 'app-search',
@@ -10,33 +8,16 @@ import { SettingService } from 'src/app/service/setting.service';
 })
 export class SearchComponent implements OnInit {
 
-  username:string
   searcharr=[];
-  constructor(private searchService:SearchService,private settingService:SettingService) { }
+  constructor(private searchService:SearchService) { }
 
   ngOnInit() {
     this.fetchdata();
-    this.addprofileurl();
-    console.log(this.searcharr)
-
   }
 
   fetchdata(){
     this.searcharr=this.searchService.searchResult();
   }
-
-  addprofileurl(){
-    console.log("Inside Search:")
-    //console.log(this.searcharr)
-    for (let user in this.searcharr){
-      this.username=this.searcharr[user].username;
-      this.settingService.getuserdata(this.username).subscribe(response=>{
-        this.searcharr[user].url=response.url
-  })
-    }
-    console.log(this.searcharr)
-  }
-  
 
 
 
