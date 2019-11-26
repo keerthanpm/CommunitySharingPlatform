93b3b0c315d65bd2f0dd6e101d11b60f936e4d69
b8ibmjava18@iiht.tech <b8ibmjava18@iiht.tech>
2019-11-26 13:08:57 +0530

    Search Completely Integrated...

--- FSDAngular/src/app/shared/components/search/search.component.ts
+++ FSDAngular/src/app/shared/components/search/search.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SearchService } from 'src/app/service/search.service';
+import { SettingService } from 'src/app/service/setting.service';
 
 @Component({
   selector: 'app-search',
@@ -7,16 +8,29 @@ import { SearchService } from 'src/app/service/search.service';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
-
+  username:string
   searcharr=[];
-  constructor(private searchService:SearchService) { }
+  constructor(private searchService:SearchService,private settingService:SettingService) { }
 
   ngOnInit() {
     this.fetchdata();
+    this.addprofileurl();
   }
 
   fetchdata(){
     this.searcharr=this.searchService.searchResult();
+  }
+
+  addprofileurl(){
+    console.log("Inside Search:")
+    //console.log(this.searcharr)
+    for (let user in this.searcharr){
+      this.username=this.searcharr[user].username;
+      this.settingService.getuserdata(this.username).subscribe(response=>{
+        this.searcharr[user].url=response.url
+  })
+    }
+    console.log(this.searcharr)
   }
 
 
