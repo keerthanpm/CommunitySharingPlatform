07cbb8724901bff94c688e9115ae133020f7d9df
arjunsuvarna1 <arjunsuvarna.s@gmail.com>
2019-11-27 15:40:03 +0530

    mongodb atlas

--- FSDNode/_helpers/error-handler.js
+++ FSDNode/_helpers/error-handler.js
@@ -3,12 +3,15 @@ module.exports = errorHandler;
 function errorHandler(err, req, res, next) {
     if (typeof (err) === 'string') {
         // custom application error
+        
         return res.status(400).json({ message: err });
     }
 
     if (err.name === 'ValidationError') {
         // mongoose validation error
+        
         return res.status(400).json({ message: err.message });
+        
     }
 
     if (err.name === 'UnauthorizedError') {
