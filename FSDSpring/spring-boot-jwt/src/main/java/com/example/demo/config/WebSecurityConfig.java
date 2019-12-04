b2e98fa596dd40614808948fe57c90f466b7bfaf
b8ibmjava18@iiht.tech <b8ibmjava18@iiht.tech>
2019-12-04 14:45:16 +0530

    Added Swagger to Spring JWT

--- FSDSpring/spring-boot-jwt/src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ FSDSpring/spring-boot-jwt/src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -54,7 +54,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/user/authenticate","/user/register").
+				.authorizeRequests().antMatchers("/user/authenticate","/user/register","/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").
 				permitAll().antMatchers(HttpMethod.OPTIONS, "/**").
 				permitAll().
 				// all other requests need to be authenticated
